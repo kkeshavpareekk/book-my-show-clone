16750e30dd25d11a58e930a97d41aa06d12b02f4
keshav pareek <kkeshavpareekk@gmail.com>
2021-11-12 19:12:34 +0530

    payment gateway done

--- src/components/MovieHero/Movieinfo.component.jsx
+++ src/components/MovieHero/Movieinfo.component.jsx
@@ -1,13 +1,31 @@
-import React, {useContext} from 'react';
+import React, {useContext, useState} from 'react';
 
 import { MovieContext } from '../../context/movie.context';
 
+//components
+import PaymentModal from '../PaymentModal/payment.component';
+
 const MovieInfo = () => {
+
+    const [isOpen , setIsOpen] = useState(false);
+    const [price, setPrice] = useState(0); 
+
     const { movie } = useContext(MovieContext);
     const genres = movie.genres?.map(({ name }) => name).join(", ");
 
+    const rentMovies = () => {
+        setIsOpen(true);
+        setPrice(149);
+    };
+
+    const buyMovies = () => {
+        setIsOpen(true);
+        setPrice(549);
+    };
+
     return (
         <>
+        <PaymentModal setIsOpen={setIsOpen} isOpen={isOpen} price={price} />
         <div className="flex flex-col gap-3">
             <div className="flex items-center gap-3 md:px-4">
                 <div className="w-40 h-8">
@@ -23,11 +41,11 @@ const MovieInfo = () => {
                     <h4>4k &bull; {movie.original_language}</h4>
                     <h4>{(movie.runtime/60).toFixed(2)} h &bull; {genres}&bull; 13+</h4>
                 </div>
-                <div className="flex items-center gap-3 md:w-screen lg:w-full md:px-4">
-                    <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+                <div className="flex items-center gap-3 md:px-4 md:w-screen lg:w-full">
+                    <button onClick={rentMovies} className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
                         Rent ₹149 
                     </button>
-                    <button className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
+                    <button onClick={buyMovies} className="bg-red-600 w-full py-3 text-white font-semibold rounded-lg">
                         Buy ₹549
                     </button>
                 </div>
--- src/components/Navbar/navbar.component.jsx
+++ src/components/Navbar/navbar.component.jsx
@@ -32,7 +32,7 @@ const NavLg = () => {
             <div className="container mx-auto px-4 flex items-center justify-between">
                 <div className="flex items-center w-1/2 gap-3">
                     <div className="w-12 h-12">
-                        <img className="w-full h-full" src="https://assets.brandfetch.io/ba050e55feb14e5.png" alt="logo" />
+                        <img className="w-full h-full" src="https://i.ibb.co/zPBYW3H/imgbin-bookmyshow-office-android-ticket-png.png" alt="logo" />
                     </div>
                     <div className="w-full flex items-center gap-3 bg-white px-3 py-2 rounded">
                         <BiSearch />
